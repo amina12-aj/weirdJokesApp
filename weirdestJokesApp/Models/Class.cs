0447de3963945c2b8fad605cf1aab0d43d8dafd7
amina12-aj <bakareaminata@gmail.com>
2022-06-10 21:22:49 +0100

    first commit

---
+++ weirdestJokesApp/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace weirdestJokesApp.Models
+{
+    public class Class
+    {
+    }
+}
---
+++ weirdestJokesApp/Models/Joke.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace weirdestJokesApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokesAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+
+    }
+}
